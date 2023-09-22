ce725c5954f005100b4c557040321dcc07eaa6cb
Ana Runje <ana.runje@clover.com>
2023-09-22 16:54:19 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JPA-pagination-tutorial
 Following JPA pagination tutorial by Dan Vega
+[Watch it on YouTube](https://www.youtube.com/watch?v=oq-c3D67WqM)
+[Follow it on the blog](https://www.danvega.dev/blog/2022/05/12/spring-data-jpa-pagination/)
---
+++ src/main/java/dev/runje/jpapaging/JpaPagingApplication.java
@@ -0,0 +1,13 @@
+package dev.runje.jpapaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPagingApplication.class, args);
+	}
+
+}
