351238329279e5968b3926dd4972b75bddb1da7a
Ana Runje <ana.runje@clover.com>
2023-09-25 11:55:25 +0100

    use java faker to populate 100 entries in the db

--- src/main/java/dev/runje/jpapaging/JpaPagingApplication.java
+++ src/main/java/dev/runje/jpapaging/JpaPagingApplication.java
@@ -1,7 +1,9 @@
 package dev.runje.jpapaging;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaPagingApplication {
@@ -9,4 +11,8 @@ public class JpaPagingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaPagingApplication.class, args);}
 
+	@Bean
+	Faker faker() {
+		return new Faker();
+	}
 }
--- src/main/java/dev/runje/jpapaging/data/SampleDataLoader.java
+++ src/main/java/dev/runje/jpapaging/data/SampleDataLoader.java
@@ -1,5 +1,6 @@
 package dev.runje.jpapaging.data;
 
+import com.github.javafaker.Faker;
 import dev.runje.jpapaging.model.Address;
 import dev.runje.jpapaging.model.Person;
 import dev.runje.jpapaging.repository.PersonRepository;
@@ -8,26 +9,42 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Component
 public class SampleDataLoader implements CommandLineRunner {
 
   private final Logger logger = LoggerFactory.getLogger(SampleDataLoader.class);
+  private final Faker faker;
   private final PersonRepository repository;
 
-  public SampleDataLoader(PersonRepository repository){
+  public SampleDataLoader(PersonRepository repository, Faker faker){
     this.repository = repository;
+    this.faker = faker;
   }
 
   @Override
   public void run(String... args) throws Exception {
     logger.info("Loading Sample Data...");
-    Person person = new Person("Ana",
-                               "Runje",
-                               "000.000.0000",
-                               "email@email.com",
-                               new Address("Steet", "City", "State", "Zip"));
 
-    repository.save(person);
+//    Create 100 rows of people in the database
+    List<Person> people = IntStream.rangeClosed(1, 100)
+            .mapToObj(i -> new Person(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.phoneNumber().phoneNumber(),
+                faker.internet().emailAddress(),
+                new Address(
+                    faker.address().streetAddress(),
+                    faker.address().city(),
+                    faker.address().state(),
+                    faker.address().zipCode()
+                )
+            ))
+            .toList();
+
+    repository.saveAll(people);
 
   }
 }
